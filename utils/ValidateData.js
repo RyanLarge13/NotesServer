31f0b3d6e3dc368c9ddf4473a1747d85732bace1
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2023-11-30 23:25:22 -0500

    Validation
    Adding a few validator methods before bed

--- utils/ValidateData.js
+++ utils/ValidateData.js
@@ -1,19 +1,54 @@
+import JSDOM from "jsdom";
+
 class Validator {
   validateHtml(html) {
-    return { valid: true };
+    try {
+      new JSDOM(html);
+      return { valid: true };
+    } catch (err) {
+      console.log(`Error in validating html: ${err}`);
+      return {
+        valid: false,
+        text: html,
+        type: "Invalid notes",
+        validType: "Please edit your notes",
+      };
+    }
   }
+
+  validateUsername(username) {
+    if (typeof username !== string) {
+      const type = typeof username;
+      return { valid: false, text: username, type: type, validType: "string" };
+    }
+    if (username.length > 20) {
+      return { valid: false, text: username, type: null, validType: null };
+    }
+    if (username.length < 4) {
+      return { valid: false, text: username, type: null, validType: null };
+    }
+  }
+
+  validateEmail() {}
+
+  validatePassword() {}
+
   validateString(text) {
     if (typeof text !== string) {
-      return { valid: false, text: text };
+      const type = typeof string;
+      return { valid: false, text: text, type: type, validType: "string" };
     }
     return { valid: true };
   }
+
   validateId(id) {
     if (typeof id !== Number) {
-      return { valid: false, text: id };
+      const type = typeof id;
+      return { valid: false, text: id, type: type, validType: "number" };
     }
     return { valid: true };
   }
+
   validateArray(arr) {
     const allValidInputs = arr.every((bool) => bool.valid === true);
     if (!allValidInputs) {
