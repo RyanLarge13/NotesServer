8ff61f30d170f18fe02270a4fadc338b2ecd3fdf
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-01-14 09:10:31 -0500

    CORS 🥵
    Production Configuration:
    Setting CORS middleware and configuring PORT

--- server.js
+++ server.js
@@ -9,7 +9,15 @@ import foldersRouter from "./routes/foldersRouter.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.DEPLOYMENT === "dev" ? 8080 : 8080;
+const PORT = process.env.PORT  || 8080;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use(cors());
 app.use(parser.json());
