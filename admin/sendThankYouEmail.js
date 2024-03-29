b53d95d3a924b01c2413ff20c2b2889e04c43378
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-03-28 22:38:38 -0400

    Sending Group Emails

--- admin/sendThankYouEmail.js
+++ admin/sendThankYouEmail.js
@@ -3,12 +3,39 @@ import { dirname } from "path";
 import fs from "fs/promises";
 import path from "path";
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const thankYouTemplatePath = path.join(
-  __dirname,
-  "../emailTemplates/thankyou.html"
+ __dirname,
+ "../emailTemplates/thankyou.html"
 );
 const thankYouEmailTemplate = await fs.readFile(thankYouTemplatePath, "utf-8");
+
+const transporter = nodemailer.createTransport({
+ service: "gmail",
+ auth: {
+  user: "electronnotes@gmail.com",
+  pass: ""
+ }
+});
+
+const sendEmail = async () => {
+ const mailOptions = {
+  from: "electronnotes@gmail.com",
+  to: "",
+  subject: "Thank You",
+  html: thankYouEmailTemplate
+ };
+ try {
+  const info = await transporter.sendMail(mailOptions);
+  console.log("Email sent: " + info.response);
+ } catch (error) {
+  console.error(error);
+ }
+};
+
+sendEmail();
