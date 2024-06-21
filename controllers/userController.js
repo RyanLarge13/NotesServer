12544c940a35baaa38a9917065f05b5c43d9b0d4
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2024-06-21 14:14:54 -0400

    Changing Return

--- controllers/userController.js
+++ controllers/userController.js
@@ -195,14 +195,13 @@ class UserController {
         const newUserData = newDbUser.rows[0];
         const newToken = signUser(newUserData);
         await sendWelcomeEmail(email, username, password);
-        resHandler.successCreate(
+        const country = searchCountry(ip);
+        await sendAdminEmailUserCreateDelete(email, username, country, true);
+        return resHandler.successCreate(
           res,
           "Successfully created your new account!",
           newToken
         );
-        const country = searchCountry(ip);
-        await sendAdminEmailUserCreateDelete(email, username, country, true);
-        return;
       } catch (err) {
         console.log(err);
         return resHandler.executingQueryError(res, err);
--- utils/Migrator.js
+++ utils/Migrator.js
@@ -1,26 +1,28 @@
 import fs from "fs";
 import path from "path";
 import pool from "./dbConnection.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
 
 class Migrator {
-  async migrate(sqlFile) {
-    const queries = fs.readFileSync(path.join(__dirname, sqlFile), "utf-8");
-    const client = await pool.connect();
-    console.log("Connection opened");
-    try {
-      const data = await client.query(queries);
-      console.log("Query executed");
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    } finally {
-      client.release();
-      console.log("Connection closed");
-    }
+ async migrate(sqlFile) {
+  const queries = fs.readFileSync(path.join(__dirname, sqlFile), "utf-8");
+  const client = await pool.connect();
+  console.log("Connection opened");
+  try {
+   const data = await client.query(queries);
+   console.log("Query executed");
+   console.log(data);
+  } catch (err) {
+   console.log(err);
+  } finally {
+   client.release();
+   console.log("Connection closed");
   }
+ }
 }
 
 export default Migrator;
