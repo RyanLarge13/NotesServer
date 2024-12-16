0067d4992e0bb27aeee0cc991e6c2d7447e5725b
= <ryanhudsonlarge13@gmail.com>
2024-12-16 08:31:31 -0500

    Pointing To Old DB

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -3,12 +3,20 @@ const { Pool } = pkg;
 import dotenv from "dotenv";
 dotenv.config();
 
+// const pool = new Pool({
+//   user: process.env.DB_USER_RW,
+//   host: process.env.DB_HOST_RW,
+//   database: process.env.DB_NAME_RW,
+//   password: process.env.DB_PASSWORD_RW,
+//   port: 21542,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER_RW,
-  host: process.env.DB_HOST_RW,
-  database: process.env.DB_NAME_RW,
-  password: process.env.DB_PASSWORD_RW,
-  port: 21542,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
 });
 
 export default pool;
